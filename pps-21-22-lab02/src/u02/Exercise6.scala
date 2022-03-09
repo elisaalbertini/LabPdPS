da9b0d88cc4fdd780ee51c8997cdd8836a3b2bf7
Elisa Albertini <elisa.albertini3@studio.unibo.it>
2022-03-09 16:58:32 +0100

    - Refactoring

--- pps-21-22-lab02/src/u02/Exercise6.scala
+++ pps-21-22-lab02/src/u02/Exercise6.scala
@@ -2,17 +2,16 @@ package u02
 
 object Exercise6 extends App :
   // va bene? non credo sia a coda
-  def fib(n: Int): Int = n match
+  /*def fib(n: Int): Int = n match
     case 0 => 0
     case 1 => 1
-    case _ => fib(n - 1) + fib(n - 2)
+    case _ => fib(n - 1) + fib(n - 2)*/
 
   //questa è tail
-  /*
   def fib(n: Int): Int =
     @annotation.tailrec
-    def tail(n: Int, prev: Int, curr: Int): Int = n match
-      case n if n <= 0 => curr
-      case _ => tail(n - 1, prev + curr, prev)
-    tail(n, 1, 0)*/
+    def tail(counter: Int, fib2: Int, fib1: Int): Int = counter match
+      case n if n <= 0 => fib1
+      case _ => tail(counter - 1, fib2 + fib1, fib2)
+    tail(n, 1, 0)
 
--- pps-21-22-lab02/test/u02/Exercise4Test.scala
+++ pps-21-22-lab02/test/u02/Exercise4Test.scala
@@ -11,14 +11,16 @@ class Exercise4Test:
     assertFalse(f1(3)(2)(1))
 
   @Test def testF2(): Unit =
-    assertTrue(f2(1)(2)(3))
-    assertFalse(f2(3)(2)(1))
+    assertTrue(f2(1, 2, 3))
+    assertFalse(f2(3, 2, 1))
 
   @Test def testF3(): Unit =
-    assertTrue(f3(1, 2, 3))
-    assertFalse(f3(3, 2, 1))
+    assertTrue(f3(1)(2)(3))
+    assertFalse(f3(3)(2)(1))
 
   @Test def testF4(): Unit =
     assertTrue(f4(1, 2, 3))
     assertFalse(f4(3, 2, 1))
+
+
 
