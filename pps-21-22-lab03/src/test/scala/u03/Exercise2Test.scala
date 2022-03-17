6c7df09790bd8b8c0042aae4c6f37e48609224e7
Elisa Albertini <elisa.albertini3@studio.unibo.it>
2022-03-17 15:16:41 +0100

    - Refactoring

--- pps-21-22-lab03/src/test/scala/u03/Exercise2Test.scala
+++ pps-21-22-lab03/src/test/scala/u03/Exercise2Test.scala
@@ -5,8 +5,8 @@ import org.junit.Assert.*
 import Exercise2.*
 import Lists.List.*
 
-class Exercise2Test {
-
+class Exercise2Test:
+  
   @Test def testDrop(): Unit =
     val s = Stream.take(Stream.iterate(0)(_ + 1))(10)
     assertEquals(Cons(6, Cons(7, Cons(8, Cons(9, Nil())))), Stream.toList(Stream.drop(s)(6)))
@@ -16,4 +16,4 @@ class Exercise2Test {
 
   @Test def testFibs(): Unit =
     assertEquals(Cons(0, Cons(1, Cons(1, Cons(2, Cons(3, Cons(5, Cons(8, Cons(13, Nil())))))))), Stream.toList(Stream.take(Stream.fibs)(8)))
-}
+
