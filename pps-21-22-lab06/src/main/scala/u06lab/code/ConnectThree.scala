03bf8cb935abfae9166d06ebd89aa2fe7fec7ce8
Elisa Albertini <elisa.albertini3@studio.unibo.it>
2022-04-21 10:14:43 +0200

    Formatting Minor fixes Refactoring method "placeAnyDisk" Refactoring method "checkVictory"

--- pps-21-22-lab06/src/main/scala/u06lab/code/ConnectThree.scala
+++ pps-21-22-lab06/src/main/scala/u06lab/code/ConnectThree.scala
@@ -7,15 +7,18 @@ import scala.annotation.tailrec
 import scala.language.postfixOps
 import scala.util.control.Breaks
 
-object ConnectThree extends App:
+object ConnectThree extends App :
   val bound = 3
+
   enum Player:
     case X, O
+
     def other: Player = this match
       case X => O
       case _ => X
 
   case class Disk(x: Int, y: Int, player: Player)
+
   /**
    * Board:
    * y
@@ -24,7 +27,7 @@ object ConnectThree extends App:
    * 2
    * 1
    * 0
-   *   0 1 2 3 <-- x
+   * 0 1 2 3 <-- x
    */
   type Board = Seq[Disk]
   type Game = Seq[Board]
@@ -33,8 +36,8 @@ object ConnectThree extends App:
 
   def find(board: Board, x: Int, y: Int): Option[Player] =
     board.find(d => d.x == x && d.y == y) match
-    case Some(a) => Some(a.player)
-    case _ => None
+      case Some(a) => Some(a.player)
+      case _ => None
 
   def firstAvailableRow(board: Board, x: Int): Option[Int] =
     board.filter(d => d.x == x).map(d => d.y).sortWith(_ > _) match
@@ -50,52 +53,52 @@ object ConnectThree extends App:
       case _ => board
 
   def placeAnyDisk(board: Board, player: Player): Seq[Board] =
-    var boards = Seq[Board]()
-    for r <- 0 to bound do
-      val newB = newBoard(board, r, player)
-      if !newB.equals(board) then boards = boards :+ newB
-    boards
+    def _placeAnyDisk(player: Player, n: Int, game: Seq[Board]): Seq[Board] =
+      n >= 0 match
+        case true => newBoard(board, n, player) match
+          case newB if game.nonEmpty && game.head.equals(newB) => _placeAnyDisk(player, n - 1, game)
+          case newB => _placeAnyDisk(player, n - 1, newB +: game)
+        case newB => game
 
-  def compareThree(p1: Option[Player], p2: Option[Player], p3: Option[Player]): Boolean =
+    _placeAnyDisk(player, bound, Seq())
+
+  private def compareThree(p1: Option[Player], p2: Option[Player], p3: Option[Player]): Boolean =
     (p1, p2, p3) match
       case (Some(a), Some(b), Some(c)) => a == b && b == c
       case _ => false
 
-  def checkHorizontal(board: Board, x:Int, y: Int): Boolean =
-    compareThree(find(board, x, y), find(board, x, y+1), find(board, x, y-1))
+  private def checkHorizontal(board: Board, x: Int, y: Int): Boolean =
+    compareThree(find(board, x, y), find(board, x, y + 1), find(board, x, y - 1))
 
-  def checkVertical(board: Board, x:Int, y: Int): Boolean =
-    compareThree(find(board, x, y), find(board, x+1, y), find(board, x-1, y))
+  private def checkVertical(board: Board, x: Int, y: Int): Boolean =
+    compareThree(find(board, x, y), find(board, x + 1, y), find(board, x - 1, y))
 
-  def checkSinisterDiagonal(board: Board, x:Int, y: Int): Boolean =
-    compareThree(find(board, x, y), find(board, x+1, y+1), find(board, x-1, y-1))
+  private def checkSinisterDiagonal(board: Board, x: Int, y: Int): Boolean =
+    compareThree(find(board, x, y), find(board, x + 1, y + 1), find(board, x - 1, y - 1))
 
-  def checkDexterDiagonal(board: Board, x:Int, y: Int): Boolean =
-    compareThree(find(board, x, y), find(board, x+1, y-1), find(board, x-1, y+1))
+  private def checkDexterDiagonal(board: Board, x: Int, y: Int): Boolean =
+    compareThree(find(board, x, y), find(board, x + 1, y - 1), find(board, x - 1, y + 1))
 
-  def checkVictory(board: Board): Boolean =
+  private def checkVictory(board: Board): Boolean =
     var won = false
-    for disk <- board do won = won || checkHorizontal(board, disk.x, disk.y)
+    board.foreach(disk => won = won || checkHorizontal(board, disk.x, disk.y)
       || checkVertical(board, disk.x, disk.y)
       || checkSinisterDiagonal(board, disk.x, disk.y)
-      || checkDexterDiagonal(board, disk.x, disk.y)
+      || checkDexterDiagonal(board, disk.x, disk.y))
     won
 
   def computeAnyGame(player: Player, moves: Int): LazyList[Game] =
     moves > 0 match
       case true =>
         for
-          game <- computeAnyGame(player.other, moves-1)
+          game <- computeAnyGame(player.other, moves - 1)
           board <- placeAnyDisk(game.head, player)
-          if !game.exists(b => checkVictory(b)) //todo così cava tutti quelli vincenti --> vanno riaggiunti in qualche modo :/
         yield
-          board +: game
+          if !checkVictory(game.head) then
+            board +: game
+          else
+            game
       case _ => LazyList(Seq(Seq()))
-
-  computeAnyGame(O, 5).foreach { g =>
-    printBoards(g)
-    println()
-  }
 
   def printBoards(game: Seq[Board]): Unit =
     for
@@ -108,3 +111,7 @@ object ConnectThree extends App:
         print(" ")
         if board == game.head then println()
 
+//  computeAnyGame(O, 4).foreach { g =>
+//    printBoards(g)
+//    println()
+//  }
